2e88094abaf85cdba624fa07fd8b308930cfadcb
jackUTS <53325480+jackUTS@users.noreply.github.com>
2023-08-24 19:15:36 +1000

    Coupon Generation

--- Source/coupon.js
+++ Source/coupon.js
@@ -1,18 +1,52 @@
-class Member {
+class Coupon {
     //Personal details
     id:             number;
-    first_name:     string;
-    last_name:      string;
-    email_address:  string;
-    password:       string;
+    voucher:        string;
+    expiry_date:    string;
+    isActive:       string;
+    amount:       string;
 
-    constructor(id: number, first_name: string, last_name: strring, email_address: string, password: string) {
+    constructor(id: number, voucher: string, expiry_date: strring, isActive: string, amount: string) {
         this.id             = id;
-        this.first_name     = first_name;
-        this.last_name      = last_name;
-        this.email_address  = email_address;
-        this.password       = password;
+        this.voucher        = voucher;
+        this.expiry_date    = expiry_date;
+        this.isActive       = isActive;
+        this.amount         = amount;
+    }
+
+    function couponGenerator() {
+        var coupon = "";
+        const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 4; i++){
+            coupon += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return coupon;
+    }
+
+    let discountExists = false;
+    do {
+        let myDicountCode = couponGenerator();
+        let newDiscountCode = new DiscountCode({voucher: myDicountCode, 
+                                                ispercent: false, 
+                                                amount: [{AUD: 5}, {USD: 10}], 
+                                                expiry: expiry_date, 
+                                                isActive: true
+    });
+        newDiscountCode.save(function (err) {
+            if (err) {
+                if (err.name === 'MongoError' && err.code === 11000) {
+                    //Duplicate detected
+                    discountExists = true;
+                }
+            }
+            res.send({
+                //success message
+            })
+        })
+        
     }
 }
 
-export = Member;
+export = Coupon;
