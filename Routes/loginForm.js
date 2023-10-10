469831ab550c54cfb0e0a6d2cf63491fad020449
Walled-S <13873068@student.uts.edu.au>
2023-10-10 22:26:47 +1100

    Update to Dependacies and Login/Registration

--- Routes/loginForm.js
+++ Routes/loginForm.js
@@ -1,4 +1,4 @@
-"use strict"
+/*"use strict"
 const bcryptjs = require("bcryptjs")
 const db = require("../Source/db-client")
 const Member = require("../Source/member")
@@ -73,4 +73,77 @@ module.exports = {
     loginPage,
     loginFormSubmit,
     logout
+}*/
+
+"use strict";
+const bcryptjs = require("bcryptjs");
+const db = require("../Source/db-client");
+const Member = require("../Source/member");
+const Venue = require("../Source/venue");
+const path = require("path");
+
+const { createConsoleLogger } = require("@nlpjs/logger");
+
+const log = createConsoleLogger();
+
+function loginPage(req, res) {
+  if (req.session.user !== undefined) {
+    return res.redirect("/dashboard");
+  }
+
+  res.set("Cache-Control", "no-store");
+  res.sendFile(path.join(__dirname, "../views/login.html"));
 }
+
+async function loginFormSubmit(req, res) {
+  try {
+    const req_email = req.body.email_address;
+    const req_password = req.body.password;
+    let isVenue = false;
+
+    const user = await db.members_collection.findOne({ "email address": req_email });
+
+    if (user === null) {
+      return res.redirect("/login");
+    }
+
+    const valid = await bcryptjs.compare(req_password, user.password);
+    const venue = await db.venues_collection.findOne({ "email address": req_email });
+
+    if (valid) {
+      log.info(`Successful login attempt [USER: ${user.email_address}]`);
+      req.session.regenerate(function () {
+        req.session.user = user;
+        req.session.isVenue = isVenue;
+        return res.redirect("/dashboard");
+      });
+    } else if (venue) {
+      isVenue = true;
+      log.info(`Successful login attempt [VENUE: ${venue.email_address}]`);
+      req.session.regenerate(function () {
+        req.session.user = user;
+        req.session.isVenue = isVenue;
+        return res.redirect("/venue-dashboard");
+      });
+    } else {
+      log.warning(`Failed login attempt with incorrect password [USER: ${user.email_address}]`);
+      return res.redirect("/login");
+    }
+  } catch (err) {
+    log.error(`Failed login due to internal error: ${err}`);
+    if (!res.writableEnded) {
+      return res.sendStatus(500);
+    }
+  }
+}
+
+function logout(req, res) {
+  req.session.destroy(function () {});
+  res.redirect("/");
+}
+
+module.exports = {
+  loginPage,
+  loginFormSubmit,
+  logout,
+};
--- Routes/registration.js
+++ Routes/registration.js
@@ -15,7 +15,7 @@ function registerPage (req, res) {
 
 // Password minimum rules
 //const passwordRegex = new RegExp("^(?=.*[A-Za-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", "i")
-
+/*
 function registerFormSubmit (req, res) {
     
         req.check(req.body.password).matches("^(?=.*[A-Za-Z])(?=.*[#$@!%&*?])(?=.*\d)[A-Za-z#$@!%&*??=.*\\d]{8,}$", "i")
@@ -36,6 +36,53 @@ function registerFormSubmit (req, res) {
                 res.redirect("/register")
             }
         }
+}
+*/
+async function registerFormSubmit(req, res) {
+  try {
+    // Validate the password
+    req.check('password').matches(
+        /^(?=.*[A-Za-z])(?=.*[#$@!%&*?])(?=.*\d)[A-Za-z#$@!%&*?0-9]{8,}$/,
+         'i');
+
+    // Check if the email already exists in the database
+    const documentCount = await db.member_collection.countDocuments({
+      email: req.body.email,
+    });
+
+    if (documentCount === 0) {
+      // Hash the password
+      const hash = await bcryptjs.hash(req.body.password, 10);
+
+      // Create a new Member instance
+      const member = new Member(
+        req.body.id,
+        req.body.first_name,
+        req.body.last_name,
+        req.body.email,
+        hash,
+        false
+      );
+
+      // Insert the member into the database
+      const result = await db.member_collection.insertOne(member);
+
+      if (result) {
+        log.info(`User Successfully added [USER: ${member.email_address}]`);
+        res.redirect('/login');
+      } else {
+        log.error('Failed to create user');
+        res.redirect('/register');
+      }
+    } else {
+      // Handle the case where the email already exists
+      res.status(400).send('Email already exists');
+    }
+  } catch (error) {
+    // Handle errors here
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 }
 
 module.exports = {
